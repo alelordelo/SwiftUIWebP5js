8abce16299c4ef5ef47d64c84e837c2e5f124196
Alexandre Lordelo <alexandre.lordelo@icloud.com>
2022-02-07 11:44:10 +0100

    Added simple P5.js example

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -4,35 +4,26 @@
 //
 //  Created by Alexandre Lordelo on 2022-02-03.
 //
-
 import SwiftUI
 
 @available(OSX 11.0, *)
 
 struct ContentView: View {
-
-  //private var url: URL? = URL(string: "https://editor.p5js.org/generative-design/sketches/P_1_0_01")
     
- private var url: URL? = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "www")
-    
-    
- //   let url = Bundle.main.url(forResource: "LocalWebsite", withExtension: "html", subdirectory: "www")
-
-//    if let url = Bundle.main.url(forResource: Index, withExtension: “html”, subdirectory: “www”) {
-//    webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
-//    }
-        
-  //      if let url = URL(string: "https://www.example.com") {
-  //          webView.load(URLRequest(url: url))
-   //     }
-
-        
-
+  func bundleURL(fileName: String, fileExtension: String) -> URL {
+    if let fileURL = Bundle.main.url(forResource: fileName, withExtension: fileExtension, subdirectory: "www") {
+      return fileURL
+    } else {
+      print("File not found")
+      return URL(string: "")!
+    }
+  }
+  
   init() {
     print("Hello World")
   }
 
   var body: some View {
-    WebView(data: WebViewData(url: self.url!))
+    WebView(data: WebViewData(url: self.bundleURL(fileName: "index", fileExtension: "html")))
   }
 }
